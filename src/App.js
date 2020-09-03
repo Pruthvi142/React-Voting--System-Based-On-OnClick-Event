2c169f8a2986efbf463384026a06fbf946adc3f3
pruthviraj <haiyalkarpruthvi@gmail.com>
2020-09-03 10:54:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			languages : [
+				{name: "JavaScript", votes: 0},
+				{name: " Rubby", votes: 0},
+				{name: "C++", votes: 0},
+				{name: "Python", votes: 0}
+			]
+		}
+	}
+
+	handlevote =(i) =>{
+    let newLanguages =this.state.languages;
+    console.log(newLanguages)
+		newLanguages[i].votes++;
+
+		this.setState({languages: newLanguages});
+		
+	}
+
+	render(){
+		return(
+			      <div className="container">
+					  <div className="row">
+					 
+						  <div className="col-md-4"></div>
+					 
+
+				<div className="col-md-4">
+				<h1>Vote Your Language!</h1>
+			   
+				   <div className="table">
+					   <table>
+						   <thead className="thead-dark">
+							   <tr>
+							   <th >Languages</th>
+							   <th>Number of Votes</th>
+							   <th >Action</th>
+							   	</tr>
+						   </thead>
+						   <tbody>
+							   {
+								   this.state.languages.map((ele,i)=>{
+									   return(
+										     <tr>
+												 <td>{ele.name}</td>
+									             <td >{ele.votes}</td>
+												 <td><button type="button" class="btn btn-primary" onClick={()=>{this.handlevote(i)}}>Vote</button></td>
+											 </tr>
+									   )
+								   })
+							   }
+						   </tbody>
+					   </table>
+				   </div>
+				   </div>
+				   </div>
+				   </div>
+			
+				
+		
+		);
+	}
 }
-
 export default App;
